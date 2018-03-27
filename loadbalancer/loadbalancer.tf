8353e524de25dcff3c6ff3f4ff234ceecb6dd6c1
Tobias Lückel <lueckel@solutiondrive.de>
2018-03-27 16:58:59 +0200

    Restructured naming of loadbalancer + add some more outputs

--- loadbalancer/loadbalancer.tf
+++ loadbalancer/loadbalancer.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "application_cluster_loadbalancer" {
+resource "aws_lb" "loadbalancer" {
   access_logs {
     bucket = "${var.application_cluster_lb_access_log_bucket}"
     prefix = "${var.application_cluster_lb_access_log_folder}/${var.application_cluster_environment}"
@@ -17,31 +17,31 @@ resource "aws_lb" "application_cluster_loadbalancer" {
   }
 }
 
-resource "aws_lb_listener" "application_cluster_listener" {
-  load_balancer_arn = "${aws_lb.application_cluster_loadbalancer.arn}"
+resource "aws_lb_listener" "loadbalancer_listener" {
+  load_balancer_arn = "${aws_lb.loadbalancer.arn}"
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.application_cluster_target_group.arn}"
+    target_group_arn = "${aws_lb_target_group.loadbalancer_default_target_group.arn}"
     type             = "forward"
   }
 }
 
-resource "aws_lb_listener" "application_cluster_listener_ssl" {
-  load_balancer_arn = "${aws_lb.application_cluster_loadbalancer.arn}"
+resource "aws_lb_listener" "loadbalancer_listener_ssl" {
+  load_balancer_arn = "${aws_lb.loadbalancer.arn}"
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "${var.application_cluster_ssl_policy}"
   certificate_arn   = "${var.application_cluster_ssl_cert_arn}"
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.application_cluster_target_group.arn}"
+    target_group_arn = "${aws_lb_target_group.loadbalancer_default_target_group.arn}"
     type             = "forward"
   }
 }
 
-resource "aws_lb_target_group" "application_cluster_target_group" {
+resource "aws_lb_target_group" "loadbalancer_default_target_group" {
   name     = "DEFAULT-${var.application_cluster_application_name}-${var.application_cluster_environment}-target-group"
   port     = "${var.application_cluster_instance_port_http}"
   protocol = "HTTP"
--- loadbalancer/output.tf
+++ loadbalancer/output.tf
@@ -1,11 +1,43 @@
-output "application_cluster_loadbalancer_id" {
-  value = "${aws_lb.application_cluster_loadbalancer.id}"
+output "loadbalancer_id" {
+  value = "${aws_lb.loadbalancer.id}"
 }
 
-output "application_cluster_loadbalancer_dns" {
-  value = "${aws_lb.application_cluster_loadbalancer.dns_name}"
+output "loadbalancer_arn" {
+  value = "${aws_lb.loadbalancer.arn}"
 }
 
-output "application_cluster_loadbalancer_zone_id" {
-  value = "${aws_lb.application_cluster_loadbalancer.zone_id}"
+output "loadbalancer_dns_name" {
+  value = "${aws_lb.loadbalancer.dns_name}"
+}
+
+output "loadbalancer_zone_id" {
+  value = "${aws_lb.loadbalancer.zone_id}"
+}
+
+output "loadbalancer_listener_id" {
+  value = "${aws_lb_listener.loadbalancer_listener.id}"
+}
+
+output "loadbalancer_listener_arn" {
+  value = "${aws_lb_listener.loadbalancer_listener.arn}"
+}
+
+output "loadbalancer_listener_ssl_id" {
+  value = "${aws_lb_listener.loadbalancer_listener_ssl.id}"
+}
+
+output "loadbalancer_listener_ssl_arn" {
+  value = "${aws_lb_listener.loadbalancer_listener_ssl.arn}"
+}
+
+output "loadbalancer_default_target_group_id" {
+  value = "${aws_lb_target_group.loadbalancer_default_target_group.id}"
+}
+
+output "loadbalancer_default_target_group_arn" {
+  value = "${aws_lb_target_group.loadbalancer_default_target_group.arn}"
+}
+
+output "loadbalancer_default_target_group_vpc_id" {
+  value = "${aws_lb_target_group.loadbalancer_default_target_group.vpc_id}"
 }
