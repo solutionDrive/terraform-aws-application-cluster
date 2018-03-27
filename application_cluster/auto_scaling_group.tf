fc50d8a395a71453bb10d2fcb939eb4a9fc251f1
Tobias Lückel <lueckel@solutiondrive.de>
2018-03-27 17:31:56 +0200

    Added listener_rules, target_groups and variables to be able to attach an auto_scaling_group to a defined loadbalancer

--- application_cluster/auto_scaling_group.tf
+++ application_cluster/auto_scaling_group.tf
@@ -23,3 +23,45 @@ resource "aws_autoscaling_group" "application_cluster_appserver_auto_scaling_gro
     propagate_at_launch = "${var.application_cluster_propagate_at_launch}"
   }
 }
+
+resource "aws_lb_listener_rule" "application_cluster_listener_rule" {
+  listener_arn = "${var.loadbalancer_listener_arn}"
+  priority = ""
+
+  action {
+    target_group_arn = "${aws_lb_target_group.application_cluster_target_group.arn}"
+    type             = "forward"
+  }
+
+  condition {
+    field = "${var.application_cluster_listener_rule_condition_field}"
+    values = "${var.application_cluster_listener_rule_condition_values}"
+  }
+}
+
+resource "aws_lb_listener_rule" "application_cluster_listener_rule" {
+  listener_arn = "${var.loadbalancer_listener_ssl_arn}"
+  priority = ""
+
+  action {
+    target_group_arn = "${aws_lb_target_group.application_cluster_target_group.arn}"
+    type             = "forward"
+  }
+
+  condition {
+    field = "${var.application_cluster_listener_rule_condition_field}"
+    values = "${var.application_cluster_listener_rule_condition_values}"
+  }
+}
+
+resource "aws_lb_target_group" "application_cluster_target_group" {
+  name     = "${var.application_cluster_application_name}-${var.application_cluster_environment}-target-group"
+  port     = "${var.application_cluster_instance_port_http}"
+  protocol = "HTTP"
+  vpc_id   = "${var.application_cluster_vpc_id}"
+}
+
+resource "aws_autoscaling_attachment" "application_cluster_autoscaling_attachment" {
+  autoscaling_group_name = "${aws_autoscaling_group.application_cluster_appserver_auto_scaling_group.id}"
+  alb_target_group_arn = "${aws_lb_target_group.application_cluster_target_group.arn}"
+}
--- application_cluster/variables.tf
+++ application_cluster/variables.tf
@@ -34,6 +34,11 @@ variable "application_cluster_environment" {
   description = "the environement of the cluster, e.g. stage or live"
 }
 
+variable "application_cluster_instance_port_http" {
+  default = 80
+  description = "the port for http on the instances"
+}
+
 variable "application_cluster_max_size" {
   default = 1
   description = "defines the max-value of the autoscaling group"
@@ -48,6 +53,31 @@ variable "application_cluster_subnet_ids" {
   type = "list"
 }
 
+variable "application_cluster_vpc_id" {
+  description = "vpc to attach target groups to"
+  type = "string"
+}
+
 variable "application_cluster_propagate_at_launch" {
   default = true
+}
+
+variable "loadbalancer_listener_arn" {
+  description = "arn of listener from loadbalancer"
+  type = "string"
+}
+
+variable "loadbalancer_listener_ssl_arn" {
+  description = "arn of ssl listener from loadbalancer"
+  type = "string"
+}
+
+variable "application_cluster_listener_rule_condition_field" {
+  description = "field on which the condition should be triggered"
+  type = "string"
+}
+
+variable "application_cluster_listener_rule_condition_values" {
+  description = "values which should be used on condition"
+  type = "list"
 }
