73dca97361c17900a52be940c47504b984872c7d
Matthias Alt <alt@solutiondrive.de>
2018-05-23 11:11:05 +0200

    only output the stuff when we have a application loadbalancer

--- loadbalancer/output.tf
+++ loadbalancer/output.tf
@@ -15,29 +15,29 @@ output "loadbalancer_zone_id" {
 }
 
 output "loadbalancer_listener_id" {
-  value = "${aws_lb_listener.loadbalancer_listener.0.id != "" ? aws_lb_listener.loadbalancer_listener.0.id : "no-value"}"
+  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_listener.loadbalancer_listener.*.id) : "no-value"}"
 }
 
 output "loadbalancer_listener_arn" {
-  value = "${aws_lb_listener.loadbalancer_listener.0.arn != "" ? aws_lb_listener.loadbalancer_listener.0.arn : "no-value"}"
+  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_listener.loadbalancer_listener.*.arn) : "no-value"}"
 }
 
 output "loadbalancer_listener_ssl_id" {
-  value = "${aws_lb_listener.loadbalancer_listener_ssl.0.id != "" ? aws_lb_listener.loadbalancer_listener_ssl.0.id : "no-value"}"
+  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_listener.loadbalancer_listener_ssl.*.id) : "no-value"}"
 }
 
 output "loadbalancer_listener_ssl_arn" {
-  value = "${aws_lb_listener.loadbalancer_listener_ssl.0.arn != "" ? aws_lb_listener.loadbalancer_listener_ssl.0.arn : "no-value"}"
+  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_listener.loadbalancer_listener_ssl.*.arn) : "no-value"}"
 }
 
 output "loadbalancer_default_target_group_id" {
-  value = "${aws_lb_target_group.loadbalancer_default_target_group.0.id != "" ? aws_lb_target_group.loadbalancer_default_target_group.0.id : "no-value"}"
+  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.id) : "no-value"}"
 }
 
 output "loadbalancer_default_target_group_arn" {
-  value = "${aws_lb_target_group.loadbalancer_default_target_group.0.arn != "" ? aws_lb_target_group.loadbalancer_default_target_group.0.arn : "no-value"}"
+  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.arn) : "no-value"}"
 }
 
 output "loadbalancer_default_target_group_vpc_id" {
-  value = "${aws_lb_target_group.loadbalancer_default_target_group.0.vpc_id != "" ? aws_lb_target_group.loadbalancer_default_target_group.0.vpc_id : "no-value"}"
+  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.vpc_id) : "no-value"}"
 }
