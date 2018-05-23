af57d88a47f3f2eaff277ce984daa3e405c7f9e7
Matthias Alt <alt@solutiondrive.de>
2018-05-23 14:55:48 +0200

    reformated outputs, so the dont be too long for one line

--- loadbalancer/output.tf
+++ loadbalancer/output.tf
@@ -15,29 +15,56 @@ output "loadbalancer_zone_id" {
 }
 
 output "loadbalancer_listener_id" {
-  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_listener.loadbalancer_listener.*.id) : "no-value"}"
+  value = "${
+    var.application_cluster_loadbalancer_type == "application" ?
+    join(" - ", aws_lb_listener.loadbalancer_listener.*.id) :
+    "no-value"
+  }"
 }
 
 output "loadbalancer_listener_arn" {
-  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_listener.loadbalancer_listener.*.arn) : "no-value"}"
+  value = "${
+    var.application_cluster_loadbalancer_type == "application" ?
+    join(" - ", aws_lb_listener.loadbalancer_listener.*.arn) :
+    "no-value"
+  }"
 }
 
 output "loadbalancer_listener_ssl_id" {
-  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_listener.loadbalancer_listener_ssl.*.id) : "no-value"}"
+  value = "${
+    var.application_cluster_loadbalancer_type == "application" ?
+    join(" - ", aws_lb_listener.loadbalancer_listener_ssl.*.id) :
+    "no-value"
+  }"
 }
 
 output "loadbalancer_listener_ssl_arn" {
-  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_listener.loadbalancer_listener_ssl.*.arn) : "no-value"}"
+  value = "${
+    var.application_cluster_loadbalancer_type == "application" ?
+    join(" - ", aws_lb_listener.loadbalancer_listener_ssl.*.arn) :
+    "no-value"
+  }"
 }
 
 output "loadbalancer_default_target_group_id" {
-  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.id) : "no-value"}"
+  value = "${var.application_cluster_loadbalancer_type == "application" ?
+    join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.id)
+    : "no-value"
+  }"
 }
 
 output "loadbalancer_default_target_group_arn" {
-  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.arn) : "no-value"}"
+  value = "${
+    var.application_cluster_loadbalancer_type == "application" ?
+    join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.arn) :
+    "no-value"
+  }"
 }
 
 output "loadbalancer_default_target_group_vpc_id" {
-  value = "${var.application_cluster_loadbalancer_type == "application" ? join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.vpc_id) : "no-value"}"
+  value = "${
+    var.application_cluster_loadbalancer_type == "application" ?
+    join(" - ", aws_lb_target_group.loadbalancer_default_target_group.*.vpc_id) :
+    "no-value"
+  }"
 }
